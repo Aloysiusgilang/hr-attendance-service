f55e23e4d8c535baa4860b13e565ceead5ed5554
Aloysius Gilang <gilangpramdya84@gmail.com>
2024-06-02 01:00:42 +0700

    env based cred

--- api/utils/storage.ts
+++ api/utils/storage.ts
@@ -1,8 +1,11 @@
 import { Storage } from "@google-cloud/storage";
 
 const storage = new Storage({
-  projectId: "gilang-storage-service",
-  keyFilename: "service-account.json",
+  projectId: process.env.GOOGLE_CLOUD_PROJECT_ID,
+  credentials: {
+    client_email: process.env.GOOGLE_CLOUD_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_CLOUD_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+  },
 });
 
 const bucketName = "hr-webapp";
